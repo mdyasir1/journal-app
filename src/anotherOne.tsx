30bd598ccab46085e3089119973af8665505562b
mdyasir1 <mdyasir4145@gmail.com>
2025-05-31 00:36:11 +0530

    newOne added

--- src/anotherOne.tsx
+++ src/anotherOne.tsx
@@ -5,46 +5,46 @@ import SpeechRecognition, {
 // import useClipboard from "react-use-clipboard";
 import { useState } from "react";
 
-const AnotherOne = () => {
-  const [textToCopy, setTextToCopy] = useState();
+const AnotherOne: React.FC = () => {
+  const [textToCopy, setTextToCopy] = useState<string>("");
   // const [isCopied, setCopied] = useClipboard(textToCopy, {
-  //     successDuration:1000
+  //   successDuration: 1000,
   // });
 
   const startListening = () =>
     SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
+
   const { transcript, browserSupportsSpeechRecognition } =
     useSpeechRecognition();
 
   if (!browserSupportsSpeechRecognition) {
-    return null;
+    return <p>Browser does not support speech recognition.</p>;
   }
 
   return (
-    <>
-      <div className="container">
-        <h2>Speech to Text Converter</h2>
-        <br />
-        <p>
-          A React hook that converts speech from the microphone to text and
-          makes it available to your React components.
-        </p>
+    <div className="container">
+      <h2>Speech to Text Converter</h2>
+      <br />
+      <p>
+        A React hook that converts speech from the microphone to text and makes
+        it available to your React components.
+      </p>
 
-        <div className="main-content" onClick={() => setTextToCopy(transcript)}>
-          {transcript}
-        </div>
-
-        <div className="btn-style">
-          {/* <button onClick={setCopied}>
-                        {isCopied ? 'Copied!' : 'Copy to clipboard'}
-                    </button> */}
-          <button onClick={startListening}>Start Listening</button>
-          <button onClick={SpeechRecognition.stopListening}>
-            Stop Listening
-          </button>
-        </div>
+      <div className="main-content" onClick={() => setTextToCopy(transcript)}>
+        {transcript}
       </div>
-    </>
+
+      <div className="btn-style">
+        {/* <button onClick={setCopied}>
+          {isCopied ? "Copied!" : "Copy to clipboard"}
+        </button> */}
+        <button onClick={startListening}>Start Listening</button>
+        <button onClick={SpeechRecognition.stopListening}>
+          Stop Listening
+        </button>
+        {textToCopy}
+      </div>
+    </div>
   );
 };
 
