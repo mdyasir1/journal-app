dfafb899e94c40de0565c9365dc1dfa1c72219f6
mdyasir1 <mdyasir4145@gmail.com>
2025-05-31 13:48:34 +0530

    final

--- src/AudioToText.tsx
+++ src/AudioToText.tsx
@@ -6,16 +6,20 @@ function AudioToText() {
   const { transcript, listening, resetTranscript } = useSpeechRecognition();
 
   const handler = () => {
-    if(!listening){
+    if (!listening) {
       SpeechRecognition.startListening({
         continuous: true,
       });
-    }SpeechRecognition.stopListening()
+    }
+    SpeechRecognition.stopListening();
   };
   return (
     <div className="flex flex-col items-center gap-3 justify-center">
-      <h1 className="text-3xl font-bold">Audio to Text</h1>
-      <div className="bg-[#d9dbf4aa] rounded-full w-16 h-16 flex justify-center items-center" onClick={handler}>
+      <h1 className="text-3xl font-bold">Audio To Text for Desktop</h1>
+      <div
+        className="bg-[#d9dbf4aa] rounded-full w-16 h-16 flex justify-center items-center"
+        onClick={handler}
+      >
         <img src="mic.svg" alt="mic" className="w-18 h-8" />
       </div>
       <p>{listening ? "Listening...." : "Tap on Mic to Speak.."}</p>
@@ -23,10 +27,10 @@ function AudioToText() {
       <p className="w-[800px] h-[200px] bg-[#d9dbf4aa] rounded-3xl p-3 text-base font-bold text-black">
         {transcript}
       </p>
-        <button className="border-[#d9dbf4aa]" onClick={resetTranscript}>
-          Reset
-        </button>
-      </div>
+      <button className="border-[#d9dbf4aa]" onClick={resetTranscript}>
+        Reset
+      </button>
+    </div>
   );
 }
 
